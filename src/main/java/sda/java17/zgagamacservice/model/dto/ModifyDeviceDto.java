7c60d95fdc82cce17d4c93268ddcbcf5fc76e999
gzwojcik <gzwojcik@icloud.com>
2019-01-31 15:42:15 +0100

    4rd przed zajeciami w czwartek 31.1.

--- src/main/java/sda/java17/zgagamacservice/model/dto/ModifyDeviceDto.java
+++ src/main/java/sda/java17/zgagamacservice/model/dto/ModifyDeviceDto.java
@@ -1,4 +1,21 @@
 package sda.java17.zgagamacservice.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ModifyDeviceDto {
+
+    private Long idToModify;
+    private String name;
+    private Double value;
+    private LocalDateTime dateAdded;
+    private String serialNumber;
+    private int productionYear;
 }
